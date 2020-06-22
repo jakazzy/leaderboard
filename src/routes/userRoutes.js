b10cab51a54db94e6aaa7e8aa5c643e2c52fe422
jakazzy <jakazzy@gmail.com>
2020-06-22 17:46:51 +0000

    add routes for uploading csv files

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,7 @@
+import multer from 'multer'
 import v1 from '../controllers'
 
+const upload = multer({ dest: 'data/' })
 export default (express) => {
     const router = express.Router()
 
@@ -7,6 +9,12 @@ export default (express) => {
         .route('/')
         .get(v1.userController.getAllUsers)
         .post(v1.userController.createUser)
+
+    router.post(
+        'upload-csv',
+        upload.single('file'),
+        v1.userController.uploadcsv
+    )
 
     //  upload csv file
     return router
