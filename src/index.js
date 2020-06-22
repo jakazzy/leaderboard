18e955df6a79b61c4baf8ea5c1356bc2ed4cd977
jakazzy <jakazzy@gmail.com>
2020-06-22 14:32:45 +0000

    parse csv data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import morgan from 'morgan'
+import parse from 'csv-parse'
+import fs from 'fs'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const csvData = []
 
+// middleware
 if (process.env.NODE_ENV === 'development') {
     app.use = morgan('dev')
 }
+fs.createReadStream('/data/test.csv')
+    .pipe(parse({ delimiter: ',' }))
+    .on('data', (data) => {
+        csvData.push(data)
+    })
+    .on('end', () => console.log(csvData))
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
