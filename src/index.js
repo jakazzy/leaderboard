d7130edd161f9d7ad9fd8c303c52bb524806993d
jakazzy <jakazzy@gmail.com>
2020-06-22 20:54:51 +0000

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 import routes from './routes'
-// import parse from 'csv-parse'
-// import fs from 'fs'
 
 const app = express()
 const PORT = process.env.PORT || 8080
-// const csvData = []
 
 // middleware
 if (process.env.NODE_ENV === 'development') {
@@ -15,12 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(cors())
 app.use('/api/v1', routes.userRoutes(express))
-// fs.createReadStream(__dirname + '/data/test.csv')
-//     .pipe(parse({ delimiter: ',', columns: true }))
-//     .on('data', (data) => {
-//         csvData.push(data)
-//     })
-//     .on('end', () => console.log(csvData))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('welcome to the parser')
